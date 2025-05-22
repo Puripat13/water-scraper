87e4e05555d025010e7eb7490903bccf33dac3f4
Puripat13 <pph656512@gmail.com>
2025-05-22 19:18:08 +0700

    Update scrap1.py

--- scrap1.py
+++ scrap1.py
@@ -12,6 +12,8 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
+options.binary_location = "/usr/bin/chromium-browser"
+
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.pwa.co.th/province/report")
 
